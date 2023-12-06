62fe14a6dac7cd091f1f00df7ebd8816ab2c264b
roberts <roberts@roberts-linux.Dlink>
2023-12-07 00:42:55 +0200

    responsive design, color change

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  showSidebar: boolean = false;
 
+  toggleSidebar(): void {
+    this.showSidebar = !this.showSidebar
+  }
 }
