9df7bad407c08c0115409f3c037c2a1f0cf9f11a
roberts <roberts@roberts-linux.Dlink>
2023-12-07 20:16:24 +0200

    shopping cart

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NgClass} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./Components/Views/home/home.component";
+import {CartComponent} from "./Components/Views/cart/cart.component";
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'cart', component: CartComponent}
 ];
